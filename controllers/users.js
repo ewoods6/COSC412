05277ec165789e497c3d555a13b04801b296c509
Eddie Woods <ewoods6@students.towson.edu>
2021-07-10 23:56:58 -0400

    Error handling for signin (passed)

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 const User = require('../models/user')
 const { errorHandler } = require('../helpers/dbErrorHandler')
+const jwt = require('jsonwebtoken')
+const expressJwt = require('express-jwt')
 
 exports.signup = (req, res) => {
     //console.log('req.body', req.body)
@@ -16,4 +18,28 @@ exports.signup = (req, res) => {
             user
         });
     });
+};
+
+exports.signin = (req, res) => {
+    // we will find user based on email
+    const { email, password } = req.body
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User with that email does not exist, please sign up!'
+            });
+        }
+        // email/pw must match
+        // create authentication method in user model
+        //generate a signed token with user id and secret
+        if(!user.authenticate(password)) {
+            return res.status(401).json({
+                error: 'Email and password dont match'
+            })
+        }
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET)
+        res.cookie('t', token, { expire : new Date() + 9999 })
+        const { _id, name, email, role } = user
+        return res.json({ token, user: { _id, email, name, role } })
+    })
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { signup } = require('../controllers/users');
+const { signup, signin } = require('../controllers/users');
 const { userSignupValidator } = require('../validator')
 
 router.post('/signup', userSignupValidator, signup);
+router.post('/signin', signin);
 
 module.exports = router;
