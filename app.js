e12274eaae6e600489ae590a50e127c1959d8b0e
Eddie Woods <ewoods6@students.towson.edu>
2021-07-10 21:43:22 -0400

    Added DB connection

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 require('dotenv').config()
 
+// app
+const app = express()
+
+// database
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then( () => console.log('DB connected')))
+
+// routes
 app.get('/', (req, res) => {
     res.send('app.js response')
 });
 
 const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+});
