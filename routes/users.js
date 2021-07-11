b12fb89fcb19b88668a27802d07363c47d727039
Eddie Woods <ewoods6@students.towson.edu>
2021-07-10 21:56:36 -0400

    Testing controllers/api

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("Hello from node")
-});
+const {sayHi} = '../controllers/user';
+
+router.get('/', sayHi);
 
 module.exports = router;
